270a50bb438bc20a6cc164ee831d19421f9516d5
Yomguithereal <guillaumeplique@gmail.com>
2016-01-11 21:20:02 +0100

    Experimenting with kotatsu

--- src/index.js
+++ src/index.js
@@ -24,10 +24,3 @@ console.warn = function () { };
 console.error = function () { };
 console.info = function () { };
 console.debug = function () { };
-
-// Listen to SIGUSR2 indicating hot updates:
-import './signal';
-
-// This is dumb but I don't understand how else to prevent process from exiting.
-// If it exits, it will get restarted by nodemon, but then hot reloading won't work.
-setInterval(() => {}, 1000);
